8bcbddc34d7cc851be32901ace8629c133021eb7
tmogoa <mogoa.tonny@gmail.com>
2021-11-10 18:47:56 +0300

    minor add

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@ int Window::height;
 int main() {
     Window window("test", 800, 600);
 
+    // renderables
     Triangle triangle;
+
+    // packing renderables
     window.renderables.push_back(&triangle);
+
+    // viola!
     window.render();
 }
